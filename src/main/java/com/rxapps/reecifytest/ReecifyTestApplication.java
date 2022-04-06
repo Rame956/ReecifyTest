d23b2b1d5a86d8394628e0b30c8366725b926ed5
Rame <Ximand931@gmail.com>
2022-04-06 23:52:55 +0300

    a

--- src/main/java/com/rxapps/reecifytest/ReecifyTestApplication.java
+++ src/main/java/com/rxapps/reecifytest/ReecifyTestApplication.java
@@ -2,12 +2,20 @@ package com.rxapps.reecifytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ReecifyTestApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication.run(ReecifyTestApplication.class, args);
